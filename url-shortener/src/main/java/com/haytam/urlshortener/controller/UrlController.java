b93b9894ca3cac66512e0e27ee3feb5a9035bf68
Haytam <haytamraba11@gmail.com>
2026-09-06 17:50:44 +0100

    feat: use configurable base URL for short URL generation

--- url-shortener/src/main/java/com/haytam/urlshortener/controller/UrlController.java
+++ url-shortener/src/main/java/com/haytam/urlshortener/controller/UrlController.java
@@ -4,17 +4,17 @@ import com.haytam.urlshortener.dto.CreateUrlRequest;
 import com.haytam.urlshortener.dto.CreateUrlResponse;
 import com.haytam.urlshortener.entity.Url;
 import com.haytam.urlshortener.service.UrlService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
 
 @RestController
 @RequestMapping("/api/v1/urls")
 public class UrlController {
 
     private final UrlService urlService;
+    @Value("${app.base-url}")
+    private String baseUrl;
 
     public UrlController(UrlService urlService) {
         this.urlService = urlService;
@@ -27,7 +27,7 @@ public class UrlController {
 
         Url url = urlService.createShortUrl(request.originalUrl());
 
-        String shortUrl = "http://localhost:8080/" + url.getShortCode();
+        String shortUrl = baseUrl + "/" + url.getShortCode();
 
         return new CreateUrlResponse(
                 url.getOriginalUrl(),
